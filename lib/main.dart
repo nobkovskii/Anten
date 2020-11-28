d7f4331053e3b53709d18f5bb26b47fba06f1443
Nobkovskii <nobuhiro_yokoyama@gfam.co.jp>
2020-11-29 01:05:14 +0900

    [ADD] 追加ボタンと画面を追加

--- lib/main.dart
+++ lib/main.dart
@@ -15,14 +15,157 @@ class AntenApp extends StatelessWidget {
       title: 'Anten',
       theme: ThemeData(
         // テーマカラー
-        primarySwatch: Color.fromARGB(255, 241, 246, 236),
+        primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       // リスト一覧画面を表示
-      home: TodoListPage(),
+      home: ListPage(),
     );
   }
 }
+
+// リスト一覧画面用Widget
+class ListPage extends StatefulWidget {
+  @override
+  _ListPageState createState() => _ListPageState();
+}
+
+class _ListPageState extends State<ListPage> {
+  // Todoリストのデータ
+  List<String> todoList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // AppBarを表示し、タイトルも設定
+      appBar: AppBar(
+        title: Text('リスト一覧'),
+      ),
+      // データを元にListViewを作成
+      body: ListView.builder(
+        itemCount: todoList.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              subtitle: Text(todoList[index]),
+              trailing: Icon(Icons.more_vert),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          // "push"で新規画面に遷移
+          // リスト追加画面から渡される値を受け取る
+          final newListText = await Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              // 遷移先の画面としてリスト追加画面を指定
+              return AddPage();
+            }),
+          );
+          if (newListText != null) {
+            // キャンセルした場合は newListText が null となるので注意
+            setState(() {
+              // リスト追加
+              todoList.add(newListText);
+            });
+          }
+        },
+        child: Icon(Icons.favorite_border_outlined),
+      ),
+    );
+  }
+}
+
+class AddPage extends StatefulWidget {
+  @override
+  _AddPageState createState() => _AddPageState();
+}
+
+class _AddPageState extends State<AddPage> {
+  // 入力されたテキストをデータとして持つ
+  String _from = '';
+  String _to = '';
+  String _msg = '';
+
+  // データを元に表示するWidget
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ありがとうを贈る'),
+      ),
+      body: Container(
+        // 余白を付ける
+        padding: EdgeInsets.all(64),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // テキスト入力
+            TextField(
+              // 入力されたテキストの値を受け取る（valueが入力されたテキスト）
+              onChanged: (String value) {
+                // データが変更したことを知らせる（画面を更新する）
+                setState(() {
+                  // データを変更
+                  _from = value;
+                });
+              },
+            ),
+            TextField(
+              // 入力されたテキストの値を受け取る（valueが入力されたテキスト）
+              onChanged: (String value) {
+                // データが変更したことを知らせる（画面を更新する）
+                setState(() {
+                  // データを変更
+                  _to = value;
+                });
+              },
+            ),
+            TextField(
+              // 入力されたテキストの値を受け取る（valueが入力されたテキスト）
+              onChanged: (String value) {
+                // データが変更したことを知らせる（画面を更新する）
+                setState(() {
+                  // データを変更
+                  _msg = value;
+                });
+              },
+            ),
+            Container(
+              // 横幅いっぱいに広げる
+              width: double.infinity,
+              // リスト追加ボタン
+              child: RaisedButton(
+                color: Colors.blue,
+                onPressed: () {
+                  // "pop"で前の画面に戻る
+                  // "pop"の引数から前の画面にデータを渡す
+                  Navigator.of(context).pop("FROM:${_from}\nTO:${_to}\nMSG:${_msg}");
+                },
+                child: Text('Send', style: TextStyle(color: Colors.white)),
+              ),
+            ),
+            Container(
+              // 横幅いっぱいに広げる
+              width: double.infinity,
+              // キャンセルボタン
+              child: FlatButton(
+                // ボタンをクリックした時の処理
+                onPressed: () {
+                  // "pop"で前の画面に戻る
+                  Navigator.of(context).pop();
+                },
+                child: Text('Cancel'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 
 // リスト一覧画面用Widget
 class TodoListPage extends StatelessWidget {
@@ -70,7 +213,7 @@ class TodoListPage extends StatelessWidget {
             }),
           );
         },
-        child: Icon(Icons.favorite),
+        child: Icon(Icons.favorite_border_outlined),
       ),
     );
   }
@@ -81,14 +224,43 @@ class TodoAddPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: FlatButton(
-          // ボタンをクリックした時の処理
-          onPressed: () {
-            // "pop"で前の画面に戻る
-            Navigator.of(context).pop();
-          },
-          child: Text('リスト追加画面（クリックで戻る）'),
+      // *** 追加する部分 ***
+      appBar: AppBar(
+        title: Text('ありがとうを贈る'),
+      ),
+      // *** 追加する部分 ***
+      body: Container(
+        // 余白を付ける
+        padding: EdgeInsets.all(64),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // テキスト入力
+            TextField(),
+            Container(
+              // 横幅いっぱいに広げる
+              width: double.infinity,
+              // リスト追加ボタン
+              child: RaisedButton(
+                color: Colors.blue,
+                onPressed: () {},
+                child: Text('Send', style: TextStyle(color: Colors.white)),
+              ),
+            ),
+            Container(
+              // 横幅いっぱいに広げる
+              width: double.infinity,
+              // キャンセルボタン
+              child: FlatButton(
+                // ボタンをクリックした時の処理
+                onPressed: () {
+                  // "pop"で前の画面に戻る
+                  Navigator.of(context).pop();
+                },
+                child: Text('Cancel'),
+              ),
+            ),
+          ],
         ),
       ),
     );
